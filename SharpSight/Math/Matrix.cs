366c4cd4531aae83de5d921d6d9a7927b8cdeaba
Matan Weissbuch <matan2050@gmail.com>
2016-03-13 22:26:27 +0200

    updated matrix class (non-generic) with operator overloads

--- SharpSight/Math/Matrix.cs
+++ SharpSight/Math/Matrix.cs
@@ -6,10 +6,10 @@ using System.Threading.Tasks;
 
 namespace SharpSight.Math
 {
-	public class Matrix<T> 
+	public class Matrix
 	{
 		#region FIELDS
-		private     T[,]       matrixData;
+		private     double[,]       matrixData;
 		private     uint[]      dimensions;
 		#endregion
 
@@ -21,13 +21,13 @@ namespace SharpSight.Math
 			dimensions[0] = nRows;
 			dimensions[1] = nCols;
 
-			matrixData = new T[dimensions[0], dimensions[1]];
+			matrixData = new double[dimensions[0], dimensions[1]];
 		}
 		#endregion
 
 
-		#region METHODS
-		public void Element(uint row, uint col, T value)
+		#region ACCESS_METHODS
+		public void Element(uint row, uint col, double value)
 		{
 			if ((row < 0) || (row > dimensions[0]) || (col < 0) || (col > dimensions[1]))
 			{
@@ -37,37 +37,126 @@ namespace SharpSight.Math
 			matrixData[row, col] = value;
 		}
 
-		public T Element(uint row, uint col)
+		public double Element(uint row, uint col)
 		{
 			return matrixData[row, col];
 		}
 		#endregion
 
 
+		#region METHODS
+		public void Ones()
+		{
+			for (uint i = 0; i < dimensions[0]; i++)
+			{
+				for (uint j = 0; j< dimensions[1]; j++)
+				{
+					Element(i, j, 1);
+				}
+			}
+		}
+
+		public void Eye()
+		{
+			for (uint i = 0; i<dimensions[0]; i++)
+			{
+				for (uint j = 0; j<dimensions[1]; j++)
+				{
+					if (i==j)
+					{
+						Element(i, j, 1);
+					}
+				}
+			}
+		}
+
+		public void Zeros()
+		{
+			for (uint i = 0; i < dimensions[0]; i++)
+			{
+				for (uint j = 0; j < dimensions[1]; j++)
+				{
+					Element(i, j, 0);
+				}
+			}
+		}
+		#endregion
+
+
 		#region OPERATOR_OVERLOADS
-		public static Matrix<T> operator +(Matrix<T> first, Matrix<T> second)
+		public static Matrix operator +(Matrix first, Matrix second)
 		{
 			// dimensions check
-			if ((first.dimensions[0] != second.dimensions[0]) || (first.dimensions[1] != second.dimensions[1]))
-			{
+			if (!CheckPairDimensions(first, second))
 				throw new Exception();	// TODO: replace with specific exceptions
-			}
 
-			Matrix<T> returned = new Matrix<T>(first.dimensions[0], first.dimensions[1]);
+			Matrix returnedMatrix = new Matrix(first.dimensions[0], first.dimensions[1]);
 			for (uint i=0; i<first.dimensions[0]; i++)
 			{
 				for (uint j=0; j<first.dimensions[1]; j++)
 				{
-					//returned.Element(i, j, first.Element(i, j) + second.Element(i, j)); 
+					returnedMatrix.Element(i, j, first.Element(i, j) + second.Element(i, j)); 
 				}
 			}
-			return returned;
+			return returnedMatrix;
 		} 
+
+
+		public static Matrix operator -(Matrix first, Matrix second)
+		{
+			// dimensions check
+			if (!CheckPairDimensions(first, second))
+				throw new Exception();  // TODO: replace with specific exceptions
+
+			for (uint i = 0; i<second.dimensions[0]; i++)
+			{
+				for (uint j = 0; j<second.dimensions[1]; j++)
+				{
+					second.Element(i, j, -second.Element(i, j));
+				}
+			}
+			return first + second;
+		}
+
+
+		public static Matrix operator *(Matrix first, Matrix second)
+		{
+			if (first.dimensions[1] != second.dimensions[0])
+				throw new Exception();
+
+			Matrix		returnedMatrix		= new Matrix(first.dimensions[0], second.dimensions[1]);
+			double      currElements		= 0;
+
+			for (uint i = 0; i < returnedMatrix.dimensions[0]; i++)
+			{
+				for (uint j = 0; j < returnedMatrix.dimensions[1]; j++)
+				{
+					for (uint k = 0; k < first.dimensions[1]; k++)
+					{
+						returnedMatrix.Element(i, j, returnedMatrix.Element(i, j) + first.Element(i, k) * second.Element(k, j));
+					}
+				}
+			}
+
+			return returnedMatrix;
+		}
 		#endregion
 
 
 		#region PROPERTIES
 
+		#endregion
+
+
+		#region PRIVATE_METHODS
+		private static bool CheckPairDimensions(Matrix a, Matrix b)
+		{ 
+			if ((a.dimensions[0] != b.dimensions[0]) || (a.dimensions[1] != b.dimensions[1]))
+			{
+				return false;
+			}
+			return true;
+		}
 		#endregion
 
 	}
