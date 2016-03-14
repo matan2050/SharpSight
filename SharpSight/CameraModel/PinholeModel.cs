1ab6d0c0f5046725887c9e85096c9856af09a386
Matan Weissbuch <matan2050@gmail.com>
2016-03-14 08:46:47 +0200

    field declaration and default constructor for PinholeModel

--- SharpSight/CameraModel/PinholeModel.cs
+++ SharpSight/CameraModel/PinholeModel.cs
@@ -3,11 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SharpSight.Math;
 
 namespace SharpSight.CameraModel
 {
 	public class PinholeModel
 	{
+		#region FIELDS
+		private		Matrix		K;
+		private		Matrix		R;
+		private		Matrix		C;
+		private		Matrix		P;
+		#endregion
 
+
+		#region CONSTRUCTORS
+		public PinholeModel()
+		{
+			K = new Matrix(3, 3);
+			R = new Matrix(3, 3);
+			C = new Matrix(3, 1);
+		}
+
+		public PinholeModel(Matrix _K, Matrix _R, Matrix _C)
+		{
+			K = _K;
+			R = _R;
+			C = _C; 
+		}
+		#endregion
 	}
 }
