f1a22111fc2baeea38a212f131e7b63b15d2a6b1
Matan Weissbuch <matan2050@gmail.com>
2016-03-14 19:52:01 +0200

    updated unit tests for Concat, Zeros, Ones, Eye and ToString

---
+++ SharpSight/Math/Tests/MatrixTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+using SharpSight.Math;
+
+namespace SharpSight.Math.Tests
+{
+	public static class MatrixTest
+	{
+		public static void TestSpecialMatrices()
+		{
+			Matrix a = new Matrix(3,3);
+			a.Ones();
+			Console.WriteLine(a.ToString());
+
+			Matrix b = new Matrix(4,4);
+			b.Zeros();
+			Console.WriteLine(b.ToString());
+
+			Matrix c = new Matrix(2,2);
+			c.Eye();
+			Console.WriteLine(c.ToString());
+		}
+
+		public static void TestConcatenation()
+		{
+			Matrix a = new Matrix(3,3);
+			a.Ones();
+
+			Matrix b = new Matrix(3,1);
+			b.Zeros();
+
+			Matrix c = a.Concat(b, true);
+			Console.WriteLine(c.ToString());
+
+			Matrix d = b.Concat(a, true);
+			Console.WriteLine(d.ToString());
+		}
+	}
+}
