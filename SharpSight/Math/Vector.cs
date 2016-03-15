a2fe13d1d86828b8957503ff076d7938523ef4dd
Matan Weissbuch <matan2050@gmail.com>
2016-03-15 09:24:46 +0200

    new Vector class as subclass of matrix (using 1 column)

---
+++ SharpSight/Math/Vector.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpSight.Math
+{
+	public class Vector : Matrix
+	{
+		public Vector(uint numElements) : base(numElements, 1)
+		{
+		}
+	}
+}
