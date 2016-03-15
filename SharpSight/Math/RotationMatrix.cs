762ba628131498372673aa0cdb57a1c929acf5c6
Matan Weissbuch <matan2050@gmail.com>
2016-03-15 19:34:59 +0200

    initial commit of RotationMatrix subclass of Matrix

---
+++ SharpSight/Math/RotationMatrix.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpSight.Math
+{
+	public class RotationMatrix : Matrix
+	{
+		#region FIELDDS
+		private     double      x;
+		private     double      y;
+		private     double      z;
+		#endregion
+
+
+		#region CONSTRUCTORS
+		public RotationMatrix(double _x, double _y, double _z) : base(3,3)
+		{
+			x = _x;
+			y = _y;
+			z = _z;
+
+			Matrix	Rx	= new Matrix(3,3);
+			Matrix	Ry	= new Matrix(3,3);
+			Matrix	Rz	= new Matrix(3,3);
+
+			Rx.Element(0, 0, 1);
+			Rx.Element(0, 1, 0);
+			Rx.Element(0, 2, 0);
+			Rx.Element(1, 0, 0);
+			Rx.Element(1, 1, System.Math.Cos(x));
+			Rx.Element(1, 2, -System.Math.Sin(x));
+			Rx.Element(2, 0, 0);
+			Rx.Element(2, 1, System.Math.Sin(x));
+			Rx.Element(2, 2, System.Math.Cos(x));
+
+			Ry.Element(0, 0, System.Math.Cos(y));
+			Ry.Element(0, 1, 0);
+			Ry.Element(0, 2, System.Math.Sin(y));
+			Ry.Element(1, 0, 0);
+			Ry.Element(1, 1, 1);
+			Ry.Element(1, 2, 0);
+			Ry.Element(2, 0, -System.Math.Sin(x));
+			Ry.Element(2, 1, 0);
+			Ry.Element(2, 2, System.Math.Cos(x));
+
+			Rz.Element(0, 0, System.Math.Cos(z));
+			Rz.Element(0, 1, -System.Math.Sin(z));
+			Rz.Element(0, 2, 0);
+			Rz.Element(1, 0, System.Math.Sin(z));
+			Rz.Element(1, 1, System.Math.Cos(z));
+			Rz.Element(1, 2, 0);
+			Rz.Element(2, 0, 0);
+			Rz.Element(2, 1, 0);
+			Rz.Element(2, 2, 1);
+
+			Matrix R = Rx*Ry*Rz;
+			MatrixData = R.MatrixData;
+		}
+		#endregion
+	}
+}
