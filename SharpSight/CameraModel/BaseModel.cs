b01fcd15021c109dfcc6805413fb6281990eb480
Matan Weissbuch <matan2050@gmail.com>
2016-03-15 09:25:35 +0200

    a base class for all camera models, demanding implementation of point to pixel and pixel to line-of-sight vector functions

---
+++ SharpSight/CameraModel/BaseModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SharpSight.Math;
+
+namespace SharpSight.CameraModel
+{
+	public abstract class BaseModel
+	{
+		#region METHODS
+		public abstract Vector PointToPixel(Vector point);
+
+		public abstract Vector PixelToVector(Vector pixel);
+		#endregion
+	}
+}
