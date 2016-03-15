159139a4db79cfa0d89b9ce18163447d2366d240
Matan Weissbuch <matan2050@gmail.com>
2016-03-16 00:04:23 +0200

    added quaternion class as vector subclass

---
+++ SharpSight/Math/Quaternion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpSight.Math
+{
+	public class Quaternion : Vector
+	{
+		#region FIELDS
+
+		#endregion
+
+
+		#region CONSTRUCTOR
+		public Quaternion(double x, double y, double z) : base(4)
+		{
+
+		}
+		#endregion
+	}
+}
