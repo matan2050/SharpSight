cb4e51f1a700c083cc2df6d11e293fa85737e684
Matan Weissbuch <matan2050@gmail.com>
2016-03-16 09:27:31 +0200

    start implementation of quaternion constructor

--- SharpSight/Math/Quaternion.cs
+++ SharpSight/Math/Quaternion.cs
@@ -9,13 +9,20 @@ namespace SharpSight.Math
 	public class Quaternion : Vector
 	{
 		#region FIELDS
-
 		#endregion
 
 
 		#region CONSTRUCTOR
-		public Quaternion(double x, double y, double z) : base(4)
+		public Quaternion(Vector axis, double angle) : base(4)
 		{
+			Vector axisNorm = axis.Normalize();
+			Element(0, 0, System.Math.Cos(angle / 2));
+			Element(1,
+				axis.Element(0) * System.Math.Sin(angle / 2));
+			Element(2,
+				axis.Element(1) * System.Math.Sin(angle / 2));
+			Element(3,
+				axis.Element(2) * System.Math.Sin(angle / 2));
 
 		}
 		#endregion
