fc71c8bc44bfc287fbab7ed903bd1f23bac0fa92
Matan Weissbuch <matan2050@gmail.com>
2016-03-20 19:41:00 +0200

    added function signature for numerical analysis matrix function (gauss-jordan)

---
+++ SharpSight/Math/Numerical/MatrixOperations.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpSight.Math.Numerical
+{
+	public static class MatrixOperations
+	{
+		public static Matrix Cholesky(Matrix A)
+		{
+			Matrix decomposed = new Matrix(A.Dimensions[0], A.Dimensions[1]);
+
+			return decomposed;
+		}
+
+
+		public static Matrix GaussJordan(Matrix A)
+		{
+			Matrix echelonForm = new Matrix(A.Dimensions[0], A.Dimensions[1]);
+
+
+
+			return echelonForm;
+		}
+	}
+}
