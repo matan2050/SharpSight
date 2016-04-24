af8dcacb4d618415cdc65305ca4088ae6d3ac601
Matan Weissbuch <matan2050@gmail.com>
2016-04-24 10:53:33 +0300

    initial structure for command pattern

---
+++ SharpSight/Math/Command.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpSight.Math
+{
+	/// <summary>
+	/// Command pattern base class for inheritive use
+	/// </summary>
+	public class Command
+	{
+		/// <summary>
+		/// Command forward execution method
+		/// </summary>
+		public virtual void Execute()
+		{
+
+		}
+
+		/// <summary>
+		/// Command backward execution method
+		/// </summary>
+		public virtual void ReverseExecution()
+		{
+
+		}
+	}
+}
