f90f4119d0e2583f10c15c3d57a1005a7067067d
Matan Weissbuch <matan2050@gmail.com>
2016-04-24 18:50:51 +0300

    initial implementation of polynomial class

---
+++ SharpSight/Polynomial.cs
@@ -0,0 +1,50 @@
+﻿using SharpSight.Math;
+
+namespace SharpSight
+{
+	public class Polynomial
+	{
+		#region FIELDS
+		private Vector	coefficients;
+		private uint    degree;
+		#endregion
+
+
+		#region CONSTRUCTORS
+		public Polynomial(Vector coeffs)
+		{
+			this.coefficients = coeffs;
+			this.degree = (uint)coefficients.MatrixData.Length;
+		}
+		#endregion
+
+
+		#region METHODS
+
+		#endregion
+
+
+		#region PROPERTIES
+		public Vector Coefficients
+		{
+			get
+			{
+				return this.coefficients;
+			}
+			set
+			{
+				this.coefficients = value;
+				this.degree = (uint)coefficients.MatrixData.Length;
+			}
+		}
+
+		public uint Degree
+		{
+			get
+			{
+				return this.degree;
+			}
+		}
+		#endregion
+	}
+}
