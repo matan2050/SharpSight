7f94da6ac263a61aad3d69722012b675bfa14be5
Matan Weissbuch <matan2050@gmail.com>
2016-04-25 10:16:07 +0300

    unit test files - new format

---
+++ MatrixConstructTests/MatrixConstructTests.cs
@@ -0,0 +1,43 @@
+﻿using SharpSight.Math;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MatrixConstructTests
+{
+	[TestClass]
+	public class MatrixConstructTests
+	{
+		[TestMethod]
+		public void BuildMatrix()
+		{
+			Matrix newMat = new Matrix(3,4);
+			newMat[0, 0] = 1;
+			newMat[0, 1] = 2;
+			newMat[0, 2] = 3;
+			newMat[0, 3] = 4;
+
+			newMat[1, 0] = 4;
+			newMat[1, 1] = 3;
+			newMat[1, 2] = 2;
+			newMat[1, 3] = 1;
+
+			newMat[2, 0] = 5;
+			newMat[2, 1] = 6;
+			newMat[2, 2] = 7;
+			newMat[2, 3] = 8;
+
+			Assert.AreEqual(newMat.MatrixData[0, 0], 1);
+			Assert.AreEqual(newMat.MatrixData[0, 1], 2);
+			Assert.AreEqual(newMat.MatrixData[0, 2], 3);
+			Assert.AreEqual(newMat.MatrixData[0, 3], 4);
+			Assert.AreEqual(newMat.MatrixData[1, 0], 4);
+			Assert.AreEqual(newMat.MatrixData[1, 1], 3);
+			Assert.AreEqual(newMat.MatrixData[1, 2], 2);
+			Assert.AreEqual(newMat.MatrixData[1, 3], 1);
+			Assert.AreEqual(newMat.MatrixData[2, 0], 5);
+			Assert.AreEqual(newMat.MatrixData[2, 1], 6);
+			Assert.AreEqual(newMat.MatrixData[2, 2], 7);
+			Assert.AreEqual(newMat.MatrixData[2, 3], 8);
+		}
+	}
+}
